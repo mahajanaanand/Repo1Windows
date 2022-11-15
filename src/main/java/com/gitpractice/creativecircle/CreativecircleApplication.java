e0a587fb45d9396e8f0e0e4e00bdb7970ebfbeb0
Anand Mahajan <mahajanaanand@gmail.com>
2022-11-15 13:18:57 +0530

    update main class

--- src/main/java/com/gitpractice/creativecircle/CreativecircleApplication.java
+++ src/main/java/com/gitpractice/creativecircle/CreativecircleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CreativecircleApplication {
 
-	public static void main(String[] args) {
+	public static void main(String... q) {
 		SpringApplication.run(CreativecircleApplication.class, args);
 	}
 
