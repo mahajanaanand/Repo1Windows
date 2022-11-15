e8b9796a77997dfef6b151ce5e4eeb4ee1bed79e
Anand <mahajanaanand@gmail.com>
2022-11-15 13:05:32 +0530

    initial commit

---
+++ src/main/java/com/gitpractice/creativecircle/CreativecircleApplication.java
@@ -0,0 +1,13 @@
+package com.gitpractice.creativecircle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreativecircleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreativecircleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitpractice/creativecircle/CreativecircleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitpractice.creativecircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreativecircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
