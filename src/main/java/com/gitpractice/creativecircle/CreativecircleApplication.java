8e026cd5545708094d4efd63761f8e0e451e2918
Anand <mahajanaanand@gmail.com>
2022-11-15 13:52:55 +0530

    main parameter changes

--- src/main/java/com/gitpractice/creativecircle/CreativecircleApplication.java
+++ src/main/java/com/gitpractice/creativecircle/CreativecircleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CreativecircleApplication {
 
 	public static void main(String... q) {
-		SpringApplication.run(CreativecircleApplication.class, args);
+		SpringApplication.run(CreativecircleApplication.class, q);
 	}
 
 }
